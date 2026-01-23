c5a86f03115a44dd6739a9f8ba94d603aff09c32
shishirkhatiwada <khatiwadaccr@gmail.com>
2026-01-23 11:28:33 +0545

    initial commit

---
+++ components/landing/hero/page.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <section className='relative pt-32 pb-20 md:pt-48 md:pb-32 px-6 overflow-hidden'>
+        <div className='max-w-4xl mx-auto text-center relative z-20'>
+            <div className='inline-flex items-center gap-2 px-3 py-1 rounded-full border border-white/10 bg-white/5 baclkdrop-blur-md mb-8 animate-float'>
+            <span className='w-1.5 h-1.5 rounded-full bg-yellow-200 shadow-[0_0_8px_rgba(34,197,84,0.4)]'></span>
+            <span className='text-xs text-zinc-300 tracking-wide font-light'>
+                Version 1.0.0 available
+            </span>
+            </div>
+
+<h1 className=''></h1>
+
+        </div>
+        </section>
+  )
+}
+
+export default Hero
---
+++ components/landing/nav/page.tsx
@@ -0,0 +1,56 @@
+import Link from "next/link";
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <nav className="fixed top-0 inset-x-0 z-50 transistion-all duration-300 backdrop-blur-sm border-b border-white/5 bg-[#030357]/50 ">
+      <div className="max-w-6xl mx-auto px-6 h-16 flex items-center justify-between ">
+        <Link href="/" className="flex items-center gap-2">
+          <div className="w-5 h-5 bg-white rounded-sm flex items-center justify-between">
+            <div className="w-2.5 h-2.5 bg-black rounded-[1px]"></div>
+          </div>
+          <span className="text-sm font-medium tracking-tight text-white/90">
+            ConvoX
+          </span>
+        </Link>
+        <div className="hidden md:flex items-center gap-8 text-sm font-light text-zinc-400">
+          <Link
+            href="#features"
+            className="hover:text-white/90 transition-colors duration-200"
+          >
+            Pricing
+          </Link>
+          <Link
+            href="#features"
+            className="hover:text-white/90 transition-colors duration-200"
+          >
+            Integration
+          </Link>
+          <Link
+            href="#features"
+            className="hover:text-white/90 transition-colors duration-200"
+          >
+            Features
+          </Link>
+        </div>
+        <div className="flex items-center gap-4">
+          <Link
+            href={"/api/auth"}
+            className="text-xs font-medium text-zinc-400 hover:text-white transition-colors"
+          >
+            Login
+          </Link>
+
+          <Link
+            href={"/api/auth"}
+            className="text-xs font-medium bg-white text-black px-4 py-2 rounded-full hover:bg-white/90 transition-colors"
+          >
+            Get Started
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
