7f864c5ffed1a8dacd1b6eb0d2310d896e463dac
shishirkhatiwada <khatiwadaccr@gmail.com>
2026-01-24 17:13:08 +0545

    03: scalekit setup

---
+++ app/api/auth/route.ts
@@ -0,0 +1,26 @@
+import scalekit from '@/lib/scalekit';
+import crypto from 'crypto';
+import { cookies } from 'next/headers';
+import { NextResponse } from 'next/server';
+export async function GET(){
+
+    try {
+        const state = crypto.randomBytes(16).toString('hex');
+        (await cookies()).set('sk_state', state, { httpOnly: true, sameSite: 'lax', path: '/' });
+
+        const redirectUri = process.env.NEXT_PUBLIC_BASE_URL!;
+
+        const options = {
+            scope: ["openid", "profile", "email", "offline_access"], state
+        }
+        
+        const authorizationUrl = scalekit.getAuthorizationUrl(redirectUri, options);
+
+        return NextResponse.redirect(authorizationUrl);
+
+
+    } catch (error) {
+        console.log(error);
+        
+    }
+}
